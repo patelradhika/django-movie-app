6f55001b61a4852c51316c351cef9c2658b05ebf
Radhika Patel <radhika.patel2206@gmail.com>
2020-05-15 17:30:28 +0530

    Added search functionality

--- movie/views.py
+++ movie/views.py
@@ -7,4 +7,15 @@ def home(request):
     movies = Movies.objects.all()
 
     frontend = {'movies': movies}
-    return render(request, 'movie/base.html', frontend)
+    return render(request, 'movie/home.html', frontend)
+
+
+def search(request):
+    if request.method == 'POST':
+        search = request.POST.get('search-movie')
+        movies = Movies.objects.filter(movie__contains=search)
+    else:
+        movies = Movies.objects.all()
+
+    frontend = {'movies': movies}
+    return render(request, 'movie/home.html', frontend)
