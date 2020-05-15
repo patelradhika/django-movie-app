2167f20587502438bc5c7067d84528fc2ce14b6d
Radhika Patel <radhika.patel2206@gmail.com>
2020-05-15 11:52:51 +0530

    WIP home page

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Movies(models.Model):
+
+    __tablename__ = 'movies'
+
+    movie = models.CharField(max_length=100, blank=False, null=False)
+    desc = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
+    pic = models.URLField(max_length=500, default="https://www.prokerala.com/movies/assets/img/no-poster-available.jpg")
+    added_date = models.DateTimeField(blank=True, null=True)
+    updated_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.movie
+
+    class Meta:
+        verbose_name_plural = "Movies"
