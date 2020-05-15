874964c8b2f8fe28e0f5f1fc0121f4a2dc71a319
Radhika Patel <radhika.patel2206@gmail.com>
2020-05-15 17:09:24 +0530

    Home page look and feel done

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .models import Movies
+
 # Create your views here.
 def home(request):
-    return render(request, 'movie/base.html')
+    movies = Movies.objects.all()
+
+    frontend = {'movies': movies}
+    return render(request, 'movie/base.html', frontend)
